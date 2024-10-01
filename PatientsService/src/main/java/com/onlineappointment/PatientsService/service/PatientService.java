6a6240b3f69ed5d1b420eb767d044c338137bcd0
Neelima Malur <neelima.malur@gmail.com>
2024-10-01 13:09:08 -0600

    Adding resilience4j configs for few APIs

--- PatientsService/src/main/java/com/onlineappointment/PatientsService/service/PatientService.java
+++ PatientsService/src/main/java/com/onlineappointment/PatientsService/service/PatientService.java
@@ -1,5 +1,6 @@
 package com.onlineappointment.PatientsService.service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import com.onlineappointment.PatientsService.entity.Patient;
 import com.onlineappointment.PatientsService.repository.PatientRepository;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @Service
 public class PatientService {
@@ -22,20 +25,29 @@ public class PatientService {
     public Optional<Patient> getPatient(Long id) {
         return patientRepository.findById(id);
     }
-
+    @CircuitBreaker(name="getAllPatientsdb",fallbackMethod ="getAllPatientsFallback" )
     public List<Patient> getAllPatients() {
         return patientRepository.findAll();
     }
-
+    public List<Patient> getAllPatientsFallback(Throwable throwable) {
+    	System.out.println("Fallback triggered for getAllPatientsFallback due to: " + throwable.getMessage());
+        return Collections.emptyList(); 
+    }
+    
     public Patient updatePatient(Long id, Patient patient) {
         patient.setId(id);
         return patientRepository.save(patient);
     }
-
+    @CircuitBreaker(name="deletePatientdb",fallbackMethod ="deletePatientFallback" )
     public void deletePatient(Long id) {
         patientRepository.deleteById(id);
     }
+    
+    public void deletePatientFallback(Long id,Throwable throwable) {
+        System.out.println("Fallback triggered for deletePatientFallback due to: "+throwable.getMessage());
+    }
 
+    @CircuitBreaker(name = "searchPatientsdb" , fallbackMethod = "searchPatientsFallback")
     public List<Patient> searchPatients(String name, Integer age, String gender) {
         List<Patient> patients = patientRepository.findAll();
 		
@@ -43,6 +55,13 @@ public class PatientService {
 		  p.getName().toLowerCase().contains(name.toLowerCase())) && (age == null ||
 		  p.getAge() == age) && (gender == null ||
 		  p.getGender().equalsIgnoreCase(gender))) .toList();
+		 
+        
+    }
+    
+    public List<Patient> searchPatientsFallback(String name, Integer age, String gender,Throwable throwable) {
+    	System.out.println("Fallback triggered for searchPatientsFallback due to: " + throwable.getMessage());
+        return Collections.emptyList(); 
 		 
         
     }
